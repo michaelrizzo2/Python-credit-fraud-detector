aeaff2a2351ebc089801b0bd9ee97a61f0e0efbb
michaelrizzo2 <m_rizzo@live.com>
2021-05-04 10:40:54 -0400

    Basice database access

---
+++ server.py
@@ -0,0 +1,9 @@
+from firebase import firebase
+from flask import Flask
+app= Flask(__name__)
+firebase_in=firebase.FirebaseApplication("https://credit-card-fraud-flask-default-rtdb.firebaseio.com/")
+
+@app.route("/")
+def home():
+    result=firebase_in.get("/Restaraunts",None)
+    return str(result)
